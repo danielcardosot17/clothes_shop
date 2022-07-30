7906fe5db2434fc8006c9b7c64f3bb7f4ca3be88
Daniel <danielcardosot17@gmail.com>
2022-07-30 19:10:35 -0300

    ShopTests: when buy item its removed from shopkeeper list (fail)

--- Assets/Tests/EditMode/ShopTests.cs
+++ Assets/Tests/EditMode/ShopTests.cs
@@ -10,6 +10,7 @@ namespace CalangoGames
     {
         MoneyManager moneyManager;
         ShopManager shopManager;
+        Shopkeeper shopkeeper;
         InventoryManager inventoryManager;
         List<Item> buyItems;
         List<Item> sellItems;
@@ -26,6 +27,7 @@ namespace CalangoGames
             moneyManager = gameObject.AddComponent<MoneyManager>();
             shopManager = gameObject.AddComponent<ShopManager>();
             inventoryManager = gameObject.AddComponent<InventoryManager>();
+            shopkeeper = gameObject.AddComponent<Shopkeeper>();
             buyItems = new List<Item>();
             sellItems = new List<Item>();
             item1 = new Item("item1", 100);
@@ -35,6 +37,8 @@ namespace CalangoGames
             buyItems.Add(item1);
             buyItems.Add(item2);
             sellItems.Add(item3);
+
+            shopkeeper.SetItemList(buyItems);
         }
 
         [TearDown]
@@ -61,6 +65,16 @@ namespace CalangoGames
             moneyManager.TryBuyItem(item2);
             // Assert
             Assert.AreEqual(expected: 100, actual: moneyManager.Money);
+        }
+        
+        [Test]
+        public void WhenBuyItemThenRemoveFromShopkeeperList()
+        {
+            // Assing
+            // Act
+            moneyManager.TryBuyItem(item1);
+            // Assert
+            Assert.AreEqual(expected: false, actual: shopkeeper.GetItemList().Contains(item1));
         }
     }
 }
